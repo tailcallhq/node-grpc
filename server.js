7b146cc82702827da7bdd2caf4fc0d47209a7adb
meskill <8974488+meskill@users.noreply.github.com>
2023-12-22 15:07:43 +0000

    refactor: simplify types for messages

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ const server = new grpc.Server();
 addReflection(server, "./descriptor_set.bin");
 
 const NEWS = [
-  { id: "1", title: "Note 1", body: "Content 1", postImage: "Post image 1" },
-  { id: "2", title: "Note 2", body: "Content 2", postImage: "Post image 2" },
-  { id: "3", title: "Note 3", body: "Content 3", postImage: "Post image 3" },
-  { id: "4", title: "Note 4", body: "Content 4", postImage: "Post image 4" },
-  { id: "5", title: "Note 5", body: "Content 5", postImage: "Post image 5" },
+  { id: 1, title: "Note 1", body: "Content 1", postImage: "Post image 1" },
+  { id: 2, title: "Note 2", body: "Content 2", postImage: "Post image 2" },
+  { id: 3, title: "Note 3", body: "Content 3", postImage: "Post image 3" },
+  { id: 4, title: "Note 4", body: "Content 4", postImage: "Post image 4" },
+  { id: 5, title: "Note 5", body: "Content 5", postImage: "Post image 5" },
 ];
 
 server.addService(newsProto.NewsService.service, {
